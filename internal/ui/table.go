2b2b6dffc70707c0f56500099840704504993032
Christophe Lambin <christophe.lambin@gmail.com>
2024-08-10 23:47:23 +0200

    formatting

--- internal/ui/table.go
+++ internal/ui/table.go
@@ -31,13 +31,12 @@ func (t *RefreshingTable) Refresh() {
 		}
 		t.SetCell(r+1, 2, tview.NewTableCell(ipAddresses[0]))
 		latency := hop.Latency()
-		if latency == 0 {
-			continue
+		if latency > 0 {
+			t.SetCell(r+1, 3, tview.NewTableCell(strconv.FormatFloat(1000*latency.Seconds(), 'f', 1, 64)+"ms").SetAlign(tview.AlignRight))
+			t.SetCell(r+1, 4, tview.NewTableCell(Gradient(latency.Seconds(), maxLatency.Seconds(), 12)))
+			loss := 1 - hop.Availability()
+			t.SetCell(r+1, 5, tview.NewTableCell(strconv.FormatFloat(100*loss, 'f', 1, 64)+"%").SetAlign(tview.AlignRight))
+			t.SetCell(r+1, 6, tview.NewTableCell(Gradient(loss, 1, 12)))
 		}
-		t.SetCell(r+1, 3, tview.NewTableCell(latency.String()))
-		t.SetCell(r+1, 4, tview.NewTableCell(Gradient(latency.Seconds(), maxLatency.Seconds(), 12)))
-		loss := 1 - hop.Availability()
-		t.SetCell(r+1, 5, tview.NewTableCell(strconv.FormatFloat(100*loss, 'f', 2, 64)+"%"))
-		t.SetCell(r+1, 6, tview.NewTableCell(Gradient(loss, 1, 12)))
 	}
 }
--- internal/ui/table_test.go
+++ internal/ui/table_test.go
@@ -11,9 +11,9 @@ import (
 
 func TestRefreshingTable_Refresh(t *testing.T) {
 	var path ping.Path
-	path.Add(1, net.ParseIP("192.168.0.1")).Measurement(true, time.Second)
+	path.Add(1, net.ParseIP("192.168.0.1")).Measurement(true, 10*time.Millisecond)
 	//path.Add(2, nil)
-	path.Add(3, net.ParseIP("192.168.0.2")).Measurement(true, 2*time.Second)
+	path.Add(3, net.ParseIP("192.168.0.2")).Measurement(true, 20*time.Millisecond)
 
 	table := RefreshingTable{Path: &path, Table: tview.NewTable()}
 	table.Refresh()
@@ -29,8 +29,8 @@ func TestRefreshingTable_Refresh(t *testing.T) {
 	}
 	assert.Equal(t, [][]string{
 		{"hop", "addr", "name", "latency", "", "loss", ""},
-		{"1", "192.168.0.1", "", "1s", "|*****-----|", "0.00%", "|----------|"},
+		{"1", "192.168.0.1", "", "10.0ms", "|*****-----|", "0.0%", "|----------|"},
 		{"2", "", "", "", "", "", ""},
-		{"3", "192.168.0.2", "", "2s", "|**********|", "0.00%", "|----------|"},
+		{"3", "192.168.0.2", "", "20.0ms", "|**********|", "0.0%", "|----------|"},
 	}, content)
 }
